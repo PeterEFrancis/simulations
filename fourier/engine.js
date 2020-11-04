2d56b68b7c99c1087698aedc971b3576864bd5c8
Peter E. Francis <franpe02@gettysburg.edu>
2020-11-03 19:58:56 -0500

    first commit

---
+++ fourier/engine.js
@@ -0,0 +1,140 @@
+
+
+
+const canvas = document.getElementById('output');
+const ctx = canvas.getContext('2d');
+
+const RIGHT_SIDE = 350;
+
+
+
+var dt;
+var delay;
+
+var updateID;
+
+var num_circles = 0;
+var radius = [];
+var theta = [];
+var d_theta = [];
+var past_heights = [];
+
+
+var changed = true;
+
+
+reset();
+
+
+function reset() {
+  num_circles = 0;
+  radius = [];
+  theta = [];
+  d_theta = [];
+  past_heights = [];
+}
+
+
+function get_settings() {
+
+  dt = Number(document.getElementById('dt').value);
+  delay = Number(document.getElementById('delay').value);
+
+  var text = document.getElementById('list').value;
+  var lines = text.split('\n');
+  for (var i = 0; i < lines.length; i++) {
+    if (lines[i].trim() != '') {
+      var info = lines[i].split(',');
+      radius[i] = Number(info[0].trim());
+      theta[i] = 0;
+      d_theta[i] = 2 * Math.PI * Number(info[1].trim()) / 100;
+      num_circles++;
+    }
+  }
+
+}
+
+function get_xy_coordinates(theta, r) {
+  return {
+    x: r * Math.cos(theta),
+    y: r * Math.sin(theta)
+  };
+}
+
+
+
+function step() {
+
+  var x = canvas.height / 2;
+  var y = canvas.height / 2;
+
+  ctx.strokeStyle = "black";
+
+  // for each circle
+  for (var i = 0; i < num_circles; i++) {
+    // draw the circle
+    ctx.beginPath();
+    ctx.arc(x, y, radius[i] , 0, 2 * Math.PI);
+    ctx.stroke();
+    // find the radius point
+    var r_point = get_xy_coordinates(theta[i], radius[i]);
+    // draw the radius
+    ctx.beginPath();
+    ctx.moveTo(x, y);
+    ctx.lineTo(x + r_point.x, y - r_point.y);
+    ctx.stroke();
+    // add to x and y
+    x = x + r_point.x;
+    y = y - r_point.y;
+    // step theta
+    theta[i] = (theta[i] + d_theta[i]) % (2 * Math.PI);
+  }
+
+  // add y to past heights
+  past_heights.push(y);
+  if (past_heights.length > RIGHT_SIDE) {
+    past_heights.shift();
+  }
+
+  // draw line to graph
+  ctx.strokeStyle = 'red';
+  ctx.beginPath();
+  ctx.moveTo(x, y);
+  ctx.lineTo(canvas.width - RIGHT_SIDE, y);
+  ctx.stroke();
+
+  // draw divider line
+  ctx.strokeStyle = 'blue';
+  ctx.beginPath();
+  ctx.moveTo(canvas.width - RIGHT_SIDE, 0);
+  ctx.lineTo(canvas.width - RIGHT_SIDE, canvas.height);
+  ctx.stroke();
+
+  // draw past heights
+  ctx.strokeStyle = 'red';
+  for (var i = 0; i < past_heights.length - 1; i++) {
+    ctx.beginPath();
+    ctx.moveTo(canvas.width - RIGHT_SIDE + i, past_heights[past_heights.length - i]);
+    ctx.lineTo(canvas.width - RIGHT_SIDE + i + 1, past_heights[past_heights.length - i - 1]);
+    ctx.stroke();
+    // ctx.fillRect(canvas.width - RIGHT_SIDE + i, , 1, 1);
+  }
+
+}
+
+
+
+function play() {
+  reset();
+  get_settings();
+  pause();
+  updateID = setInterval(function() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    step();
+  }, delay);
+}
+
+
+function pause() {
+  clearInterval(updateID);
+}
---
+++ two-planet-orbit/engine.js
@@ -0,0 +1,95 @@
+
+
+const canvas = document.getElementById('output');
+const ctx = canvas.getContext('2d');
+
+
+var r;
+var w;
+var dt;
+var delay;
+var updateID;
+var theta;
+var d_theta;
+
+
+var changed = true;
+
+
+
+
+
+function clear() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  theta = [0,0];
+}
+
+
+function get_settings() {
+
+  var ra = Number(document.getElementById('ra').value);
+  var rb = Number(document.getElementById('rb').value);
+
+  r = [Math.min(ra, rb), Math.max(ra, rb)];
+
+  var wa = Number(document.getElementById('wa').value);
+  var wb = Number(document.getElementById('wb').value);
+
+  w = r[0] == ra ? [wa, wb] : [wb, wa];
+
+  dt = Number(document.getElementById('dt').value);
+  delay = Number(document.getElementById('delay').value);
+
+  r = [r[0] / r[1], 1];
+
+  d_theta = [
+    dt * w[0],
+    dt * w[1]
+  ];
+
+}
+
+function get_canvas_coordinates(theta, r) {
+  return {
+    x: canvas.width / 2 + r * canvas.width * 0.4 * Math.cos(theta),
+    y: canvas.height / 2 - r * canvas.height * 0.4 * Math.sin(theta)
+  };
+}
+
+
+
+function step() {
+  ctx.strokeStyle = "white";
+  ctx.beginPath();
+  var a = get_canvas_coordinates(theta[0], r[0]);
+  var b = get_canvas_coordinates(theta[1], r[1]);
+  ctx.moveTo(a.x, a.y);
+  ctx.lineTo(b.x, b.y);
+  ctx.stroke();
+
+  for (var i = 0; i < 2; i++){
+    theta[i] = (theta[i] + d_theta[i]) % (Math.PI * 2);
+  }
+}
+
+
+function play() {
+  if (changed) {
+    clear();
+    changed = false;
+  }
+  get_settings();
+  ctx.strokeStyle = "blue";
+  for (var i = 0; i < 2; i++) {
+    ctx.beginPath();
+    ctx.arc(canvas.width / 2, canvas.height / 2, canvas.width * r[i] * 0.4 , 0, 2 * Math.PI);
+    ctx.stroke();
+  }
+  updateID = setInterval(step, delay)
+
+}
+
+
+function pause() {
+  clearInterval(updateID);
+}
